fcd71a8dd47560090ce61bb5e1adf2da00c8c5a5
siwo81 <98247692+siwo81@users.noreply.github.com>
2022-03-04 18:37:53 +0700

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -1,7 +1,7 @@
 cd /usr/local/bin
 sudo wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-static-x64.tar.gz
 sudo tar xvzf xmrig-6.13.1-linux-static-x64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.13.1/xmrig -o pool.hashvault.pro:3333 -u https://monero.hashvault.pro/en/dashboard/89w9kcG7B4Z875AauqCpTcMJnf6fwzV7EDGNjfBLbwygKy2eLNrF9MfXEvJ5REFpVPdLdvgq7QTJ5d5ifKuMNCiS6aCbQ22 --rig-id vm1 --randomx-no-rdmsr\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.13.1/xmrig -o pool.hashvault.pro:3333 -u 89w9kcG7B4Z875AauqCpTcMJnf6fwzV7EDGNjfBLbwygKy2eLNrF9MfXEvJ5REFpVPdLdvgq7QTJ5d5ifKuMNCiS6aCbQ22 --rig-id vm1 --randomx-no-rdmsr\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 sudo systemctl start xmrig.service
